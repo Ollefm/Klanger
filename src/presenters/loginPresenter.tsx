768435405e0a8a73fd4a2fea1af3e8be68acab3b
Love <love.engstromer@gmail.com>
2025-04-04 13:48:17 +0200

    typo fix

--- src/presenters/loginPresenter.tsx
+++ src/presenters/loginPresenter.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react-lite"
-import { LoginView } from "../views/LoginView"
+import { LoginView } from "../views/loginView"
 export const Login = observer(function LoginRender(props) {
     return (
         <LoginView/>
