6a3bc38190264ccab5a3f60e4e2afb2ff19c9486
Leo Karsikas <leoka@kth.se>
2025-05-12 16:55:16 +0200

    migrating to quizModel

---
+++ src/presenters/InstructionsPresenter.tsx
@@ -0,0 +1,9 @@
+import { observer } from "mobx-react-lite";
+import InstructionView from "../views/instructionView";
+
+export default observer(function InstructionRender() {
+
+  return (
+    <InstructionView/>
+  );
+});
